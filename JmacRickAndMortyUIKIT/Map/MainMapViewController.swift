520ffbcd57a9129137e7c96871e301882b255b52
Jmac Logistics LLC <84051913+jmaclogisticsllc@users.noreply.github.com>
2023-03-03 12:24:49 -0600

    Added the ability for the searchBar to be displayed when the user clicks on it and dismisses when the user hits the return button

--- JmacRickAndMortyUIKIT/Map/MainMapViewController.swift
+++ JmacRickAndMortyUIKIT/Map/MainMapViewController.swift
@@ -13,24 +13,66 @@ class MainMapViewController: UIViewController {
     
     lazy var mapView: MKMapView = {
         let map = MKMapView()
-        map.showsUserLocation = true
+       // map.showsUserLocation = true
         map.translatesAutoresizingMaskIntoConstraints = false
         return map
+    }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.tintColor = .black
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.green
+        searchTextField.delegate = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(searchTextFieldTapped))
+        searchTextField.isUserInteractionEnabled = true
+
+        searchTextField.addGestureRecognizer(tapGesture)
+        
+        // Setup the MapUI
         setupUI()
+        
+
     }
     
     func setupUI() {
+        view.addSubview(searchTextField)
         view.addSubview(mapView)
+        view.bringSubviewToFront(searchTextField)
+        
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
         
         mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    @objc func searchTextFieldTapped() {
+        searchTextField.becomeFirstResponder()
+    }
+}
+
+extension MainMapViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == searchTextField {
+            textField.resignFirstResponder()
+        }
+        return false
     }
 }
